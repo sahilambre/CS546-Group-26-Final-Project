0c85b964d950de25c0845b36c60a7a4f9741a432
whittle0 <twhittle@stevens.edu>
2023-03-22 13:05:46 -0400

    Update app.js
    alright i think the connection is good, maybe now i can publish this branch, next to add data functions probably,

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//yea
+import {dbConnection, closeConnection} from './config/mongoConnection.js';
+
 //lets drop the database each time this is run
 const db = await dbConnection();
 await db.dropDatabase();
@@ -7,3 +8,8 @@ await db.dropDatabase();
 let job1 = undefined;
 
 //next to test the create function
+
+
+//finally to close up shop,, get back to the terminal after doing what u need to do
+await closeConnection();
+console.log('Done!');

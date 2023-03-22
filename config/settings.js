093632f18e0ae83ac6c1e0a1a4fd38c1ab23e5a7
whittle0 <twhittle@stevens.edu>
2023-03-22 12:58:20 -0400

    create config directory
    added the config directory to establish connection with database, though i havent rlly tested it im just nervous lol

---
+++ config/settings.js
@@ -0,0 +1,5 @@
+// YOU WILL NEED TO CHANGE THE DB NAME TO MATCH THE REQUIRED DB NAME IN THE ASSIGNMENT SPECS!!!
+export const mongoConfig = {
+  serverUrl: 'mongodb://127.0.0.1/',
+  database: 'g26_final'
+};

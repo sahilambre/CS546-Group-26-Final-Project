6c0d67c3dbe8afd1645f86b332e02c3e69593e9f
whittle0 <55468978+whittle0@users.noreply.github.com>
2023-03-29 15:26:01 -0400

    Jobfeed (#3)
    * Update app.js
    
    * jobfeed things
    
    alright i made a data folder and i put in some functions for posting job listings. so far only create and get. the create method could use some more testing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import {dbConnection, closeConnection} from './config/mongoConnection.js';
+import jobData from './data/jobs.js';
 
 //lets drop the database each time this is run
 const db = await dbConnection();
@@ -8,8 +9,12 @@ await db.dropDatabase();
 let job1 = undefined;
 
 //next to test the create function
+try {
+    job1 = await jobData.create('software dev', 'Nokia', 'https://www.nokia.com', ['computer science', 'cyber security']);
+} catch(e) {
+    console.error('Could not add job1:',e);
+}
 
-
-//finally to close up shop,, get back to the terminal after doing what u need to do
+//finally to close up shop,, get back to the terminal after doing what u need to do in the db
 await closeConnection();
 console.log('Done!');
---
+++ data/jobs.js
@@ -0,0 +1,77 @@
+import {jobs} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+
+const create = async (
+    name,
+    company,
+    website,
+    tags
+) => {
+    //name
+    if (!name) throw 'You must provide a name for your job listing';
+    if (typeof name !== 'string') throw 'Job name must be a string';
+    if (name.trim().length === 0)
+        throw 'Job name cannot be an empty string or string with just spaces';
+    name = name.trim();
+    //company
+    if (!company) throw 'You must provide a company name';
+    if (typeof company !== 'string') throw 'Company name must be a string';
+    if (company.trim().length === 0)
+        throw 'Company name cannot be an empty string or string with just spaces';
+    company = company.trim();
+    //website***
+    if (!website) throw 'You must provide a company website';
+    if (typeof website !== 'string') throw 'Company website must be a string';
+    if (website.trim().length === 0)
+        throw 'Company website cannot be an empty string or string with just spaces';
+    website = website.trim();
+    //If website does not contain https://www. and end in a .com,
+    //and have at least 5 characters in-between the https://www. and .com this method will throw.
+    let index1 = website.indexOf('https://www.');
+    let index2 = website.lastIndexOf('.com');
+    if (!website.endsWith('.com') || index1 < 0 || index2 <= index1+16)
+        throw 'Website must have http://www. and must end in .com with at least 5 characters in between';
+    //tags
+    if (!tags || !Array.isArray(tags)) throw 'You must provide an array of tags';
+    if (tags.length === 0) throw 'You must supply at least one tag';
+    for (let i in tags) {
+        if (typeof tags[i] !== 'string' || tags[i].trim().length === 0) {
+            throw 'One or more tags is not a string or is an empty string';
+        }
+        tags[i] = tags[i].trim();
+    }
+
+    let newJob = {
+        name: name,
+        company: company,
+        website: website,
+        tags: tags
+    };
+    const jobCollection = await jobs();
+    const insertInfo = await jobCollection.insertOne(newJob);
+    if (!insertInfo.acknowledged || !insertInfo.insertedId) throw 'Unable to add job listing';
+    
+    const newId = insertInfo.insertedId.toString();
+    const job = await get(newId);
+    return job;
+};
+
+const get = async (id) => {
+    if (!id) throw 'You must provide an id to search for';
+    if (typeof id !== 'string') throw 'Id must be a string';
+    if (id.trim().length === 0)
+        throw 'Id cannot be an empty string or just spaces';
+    id = id.trim();
+    if (!ObjectId.isValid(id)) throw 'invalid object ID';
+    const jobCollection = await jobs();
+    const jobW = await jobCollection.findOne({_id: new ObjectId(id)});
+    if (jobW === null) throw 'No job listings with that id';
+    jobW._id = jobW._id.toString();
+    return jobW;
+};
+
+const exportedMethods = {
+    create,
+    get
+}
+export default exportedMethods;

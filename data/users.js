75221e9e7a19731f0c7ff4ea962133be2270ee6b
HarshilCodes <38358248+HarshilCodes@users.noreply.github.com>
2023-04-08 17:44:44 -0400

    Some functionality added to users

--- data/users.js
+++ data/users.js
@@ -1,32 +1,57 @@
 import { users } from "../config/mongoCollections.js";
-import {ObjectId} from 'mongodb';
+import { ObjectId } from 'mongodb';
+import bcryptjs from 'bcryptjs';
 
-//Valid Portion 
-function validID(id){
-    if (!id) throw "id is not defined";
-    if (id.constructor !== String) throw "id has to be a string";
-    if (!ObjectId.isValid(id)) throw "id is not valid";
-  }
-  
-  function validUsername(username){
-    if (!username) throw "username is undefinded";
-    if (username.constructor !== String) throw "username is not of the proper type";
-    if (username.length < 6 ) throw "length of username is less than 6";
-    let letterNumber = /^[0-9a-zA-Z]+$/;
-    if (!username.match(letterNumber)) throw "username should contain only letter and number";
-  }
-  
-  function validPassword(password){
-    if (!password) throw "password is undefinded";
-    if (password.constructor !== String) throw "password is not of the proper type";
-    if (password.length < 8 ) throw "length of password is less than 8";
-    if (password.length > 31 ) throw "length of password is greater than 31";
-  }
+function validID(id) {
+  if (!id) { throw "id is not defined"; }
+  if (id.constructor !== String) { throw "id must be a string"; }
+  if (!ObjectId.isValid(id)) { throw "id is invalid"; }
+}
 
-  async function addUsers(username,newPassword){
-    validUsername(username);
-    validPassword(password);
-    
-  }
+function validUsername(username) {
+  if (!username) { throw "username is not defined"; }
+  if (username.constructor !== String) { throw "username must be a string"; }
+  if (username.length < 6) { throw "username length must be at least 6 characters"; }
+  let letterNumber = /^[0-9a-zA-Z]+$/;
+  if (!username.match(letterNumber)) { throw "username can only contain letters and numbers"; }
+}
+
+function validPassword(password) {
+  if (!password) { throw "password is not defined"; }
+  if (password.constructor !== String) { throw "password must be a string"; }
+  if (password.length < 8) { throw "password length must be at least 8 characters"; }
+  if (password.length > 31) { throw "password length must not exceed 31 characters"; }
+}
 
 
+async function addUsers(newUsername, newPassword) {
+  validUsername(newUsername);
+  validPassword(newPassword);
+
+  const usersCollections = await users();
+  const doesExist = await usersCollections.findOne({ username: newUsername });
+
+  if (doesExist) {
+    throw "This username already exists!";
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcryptjs.hash(newPassword, 12);
+  } catch (e) {
+    throw "Error hashing password!";
+  }
+
+  let user = {
+    username: newUsername,
+    password: hashedPassword,
+  };
+
+  const newInfo = await usersCollections.insertOne(user);
+
+  if (newInfo.insertedCount === 0) {
+    throw "New user was not created!";
+  }
+
+  return newInfo.insertedId;
+}

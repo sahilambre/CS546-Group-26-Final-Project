2bfd44a78792d47051128f758c5e1b6711ff1c8a
whittle0 <twhittle@stevens.edu>
2023-04-12 09:23:50 -0400

    Create recruiters.js
    with some of the signatures to be implemented later

---
+++ data/recruiters.js
@@ -0,0 +1,23 @@
+const create = async (
+    name,
+    otherFields) => {};
+
+const getAll = async () => {};
+
+const get = async (recruiterId) => {};
+
+const remove = async (recruiterId) => {};  
+
+const update = async (
+    recruiterId,
+    name,
+    otherFields) => {};
+
+const exportedMethods = {
+    create,
+    getAll,
+    get,
+    remove,
+    update,
+};
+export default exportedMethods;

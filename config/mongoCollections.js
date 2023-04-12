50ba94d59783056b1b2deb1519a6435ae1dfc7eb
whittle0 <twhittle@stevens.edu>
2023-04-12 18:01:08 -0400

    Update mongoCollections.js
    added recruiters db into collections file

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -19,3 +19,4 @@ const getCollectionFn = (collection) => {
 export const jobs = getCollectionFn('jobs');
 export const users = getCollectionFn("users");
 export const applicants = getCollectionFn("applicants");
+export const recruiters = getCollectionFn("applicants");

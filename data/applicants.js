375bc25b578b973d1d4a35864a1c3d647c644b63
whittle0 <twhittle@stevens.edu>
2023-04-13 22:04:04 -0400

    applicants create function progress graaa
    it aint much but its honest work

--- data/applicants.js
+++ data/applicants.js
@@ -2,17 +2,36 @@ import {applicants} from '../config/mongoCollections.js';
 import {ObjectId} from 'mongodb';
 
 const create = async (
-    name,
+    firstName,
+    lastName,
     email,
     age,
     state,
     gradYr
 ) => {
-    //name
-
+    //firstName
+    if (!firstName) throw 'You must enter your name';
+    if (typeof firstName !== 'string') throw 'Name must be a string';
+    if (firstName.trim().length === 0)
+        throw 'Name cannot be an empty string or string with just spaces';
+    firstName = firstName.trim();
+    //lastName
+    if (!lastName) throw 'You must enter your Last name';
+    if (typeof lastName !== 'string') throw 'Last name must be a string';
+    if (lastName.trim().length === 0)
+        throw 'Last name cannot be an empty string or string with just spaces';
+    lastName = lastName.trim();
+    /*in the name params, i want to add smth that keeps ppl from entering numbers in the string
+    no one has numbers in their name
+    unless ur like John Doe the fourth, in which case you can use roman numerals
+    */
     //email
 
     //age
+    if (!age) throw 'You must enter your age';
+    if (typeof age !== 'number') throw 'Age must be a number';
+    //oh also handle the NAN case
+    //also probably restrict the age?? like if ur over 80 please retire
 
     //state
 

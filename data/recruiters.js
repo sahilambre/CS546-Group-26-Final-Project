a20d354500e68ab028d67c50ac0cae37c028f4a0
whittle0 <twhittle@stevens.edu>
2023-04-25 19:46:18 -0400

    Update recruiters.js

--- data/recruiters.js
+++ data/recruiters.js
@@ -2,7 +2,16 @@ const create = async (
     name,
     otherFields) => {};
 
-const getAll = async () => {};
+const getAll = async () => {
+    const recsCollection = await recruiters();
+    let recList = await recsCollection.find({}).toArray();
+    if (!recList) throw 'Could not get recruiters';
+    recList = recList.map((element) => {
+        element._id = element._id.toString();
+        return element;
+    });
+    return recList;
+};
 
 const get = async (recruiterId) => {};
 

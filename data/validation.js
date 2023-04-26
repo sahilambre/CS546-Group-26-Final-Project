9cd827373cfc70fe4e73d3f86840bfe2b51b28b4
whittle0 <twhittle@stevens.edu>
2023-04-26 12:47:39 -0400

    Update validation.js
    added email validation

--- data/validation.js
+++ data/validation.js
@@ -35,6 +35,13 @@ const exportedMethods = {
       arr[i] = arr[i].trim();
     }
     return arr;
+  },
+
+  checkEmail(email) {
+    if (!email) throw "Email Address must be supplied";
+    let nEmailAddress = email.toLowerCase();
+    if (!(/\S+@\S+\.\S+/.test(nEmailAddress))) throw "Email address is not valid.";
+    return nEmailAddress;
   }
 };
 

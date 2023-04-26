a86354080ccd41bf9e23780350066bad48d0d334
whittle0 <twhittle@stevens.edu>
2023-04-26 13:31:32 -0400

    Update applicants.js
    rewrote applicant create function

--- data/applicants.js
+++ data/applicants.js
@@ -10,38 +10,27 @@ const create = async (
     state,
     gradYr
 ) => {
-    //TODO: complete input validation
-    //firstName
-    if (!firstName) throw 'You must enter your name';
-    if (typeof firstName !== 'string') throw 'Name must be a string';
-    if (firstName.trim().length === 0)
-        throw 'Name cannot be an empty string or string with just spaces';
-    firstName = firstName.trim();
-    //lastName
-    if (!lastName) throw 'You must enter your Last name';
-    if (typeof lastName !== 'string') throw 'Last name must be a string';
-    if (lastName.trim().length === 0)
-        throw 'Last name cannot be an empty string or string with just spaces';
-    lastName = lastName.trim();
-
-    //i want to use the helper functions to write this input validation
-
-    /*in the name params, i want to add smth that keeps ppl from entering numbers in the string
+    firstName = validation.checkString(firstName, 'first name');
+    lastName = validation.checkString(lastName, 'last name');
+    /*in the name params, i want to add smth that keeps ppl from entering ANY numbers in the string
     no one has numbers in their name
     unless ur like John Doe the fourth, in which case you can use roman numerals
     */   
-    //email
+    email = validation.checkEmail(email);
 
     //age
     if (!age) throw 'You must enter your age';
     if (typeof age !== 'number' || age == NaN) throw 'Age must be a number';
-    //also we gotta restrict the age, users must be 12 and up
+    //we gotta restrict the age, users must be 12 and up
     //also if ur over 80 please retire or smth
     if (age < 12 || age > 80) throw 'Invalid age';
 
-    //state
+    state = validation.checkString(state, 'state');
 
     //gradYr
+    if (!gradYr) throw 'You must enter your graduation year';
+    if (typeof gradYr !== 'number' || gradYr == NaN) throw 'Graduation year must be a number';
+    if (gradYr < 1950 || gradYr > 2030) throw 'Invalid graduation year';
 
     let newApp = {
         firstName: firstName,

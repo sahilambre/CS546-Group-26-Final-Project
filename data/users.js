4b1ae99bd6b522181f9fccd2c9a7598fcfc1f0b5
whittle0 <55468978+whittle0@users.noreply.github.com>
2023-04-29 14:20:03 -0400

    Data (#7)
    * some functinality for the users
    
    * Some functionality added to users
    
    * another addition
    
    * Update jobs.js
    
    added a few job data functions
    
    * created applicants data file
    
    created applicants file complete with signatures yessir (im fearful for how long these will take to implement)
    
    * Create recruiters.js
    
    with some of the signatures to be implemented later
    
    * Update mongoCollections.js
    
    added recruiters db into collections file
    
    * Update jobs.js
    
    wrote getalljobs, yet to test
    
    * Update jobs.js
    
    short note abt other functions we need to implement
    
    * Update jobs.js
    
    wrote remove
    
    * Update jobs.js
    
    edited the create function
    
    * Update jobs.js
    
    brain fart
    
    * Update applicants.js
    
    updated applicant create params
    
    * applicants create function progress graaa
    
    it aint much but its honest work
    
    * Create validation.js
    
    aughhhgh
    
    * Update applicants.js
    
    * jobs data functions update
    
    still yet to implement some things, and still yet to test all this mess
    
    * jobs db strategizing
    
    heheheh
    
    * jobs db strategizing
    
    heheheh
    
    * Update applicants.js
    
    planning to rewrite these functions using the validation helper functions
    
    * moved validation into data dir
    
    * Update applicants.js
    
    added favoriteJob signature
    
    * Update applicants.js
    
    changed the location of validation file, so i changed the import line
    
    * Update jobs.js
    
    changed the location of validation helper functions file, so i changed the import line
    
    * Update applicants.js
    
    wrote create function, input validation incomplete
    
    * changed some input validation aspects
    
    * Update validation.js
    
    * Update applicants.js
    
    update to create function, input validation not complete
    
    * progress progress
    
    little by little
    
    * Update applicants.js
    
    had a variable name wrong my b
    
    * Update applicants.js
    
    * var name error my b
    
    * Update applicants.js
    
    wrote remove function
    
    * Update jobs.js
    
    few input validation changes just want to push before i forget bc im thinking rn
    
    * Update recruiters.js
    
    * Update recruiters.js
    
    create function prep
    
    * Update recruiters.js
    
    fields etc
    
    * Update recruiters.js
    
    to match harshil's version from the main branch
    
    * Update validation.js
    
    added email validation
    
    * Update validation.js
    
    added email validation
    
    * Update recruiters.js
    
    i dont use a "helper" file, my validation helper functions are in the "validation" file
    
    * Update recruiters.js
    
    cleanin up
    
    * Update jobs.js
    
    workin on getJobsApplied
    
    * Update jobs.js
    
    cleanin up, gameplan comments etc
    
    * Update applicants.js
    
    rewrote applicant create function
    
    * Update applicants.js
    
    * Update jobs.js
    
    * file import update
    
    ---------
    
    Co-authored-by: HarshilCodes <38358248+HarshilCodes@users.noreply.github.com>

---
+++ data/users.js
@@ -0,0 +1,71 @@
+import { users } from "../config/mongoCollections.js";
+import { ObjectId } from 'mongodb';
+import bcryptjs from 'bcryptjs';
+
+function validID(id) {
+  if (!id) { throw "id is not defined"; }
+  if (id.constructor !== String) { throw "id must be a string"; }
+  if (!ObjectId.isValid(id)) { throw "id is invalid"; }
+}
+
+function validUsername(username) {
+  if (!username) { throw "username is not defined"; }
+  if (username.constructor !== String) { throw "username must be a string"; }
+  if (username.length < 6) { throw "username length must be at least 6 characters"; }
+  let letterNumber = /^[0-9a-zA-Z]+$/;
+  if (!username.match(letterNumber)) { throw "username can only contain letters and numbers"; }
+}
+
+function validPassword(password) {
+  if (!password) { throw "password is not defined"; }
+  if (password.constructor !== String) { throw "password must be a string"; }
+  if (password.length < 8) { throw "password length must be at least 8 characters"; }
+  if (password.length > 31) { throw "password length must not exceed 31 characters"; }
+}
+
+
+async function addUsers(newUsername, newPassword) {
+  validUsername(newUsername);
+  validPassword(newPassword);
+
+  const usersCollections = await users();
+  const doesExist = await usersCollections.findOne({ username: newUsername });
+
+  if (doesExist) {
+    throw "This username already exists!";
+  }
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcryptjs.hash(newPassword, 12);
+  } catch (e) {
+    throw "Error hashing password!";
+  }
+
+  let user = {
+    username: newUsername,
+    password: hashedPassword,
+  };
+
+  const newInfo = await usersCollections.insertOne(user);
+
+  if (newInfo.insertedCount === 0) {
+    throw "New user was not created!";
+  }
+
+  return getUser(newInfo.insertedId.toString());
+}
+
+async function getUser(id) {
+  validID(id);
+
+  const usersCollection = await users();
+  const user = await usersCollection.findOne({ _id: ObjectId(id) });
+
+  if (!user) {
+    throw "User not found!";
+  }
+
+  return user;
+}
+

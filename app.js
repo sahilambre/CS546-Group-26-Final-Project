875abc4c8ae4fd4ef83fe9c5afa12df55bd0d3e5
whittle0 <55468978+whittle0@users.noreply.github.com>
2023-04-29 14:20:03 -0400

    Data (#7)
    * some functinality for the users
    
    * Some functionality added to users
    
    * another addition
    
    * Update jobs.js
    
    added a few job data functions
    
    * created applicants data file
    
    created applicants file complete with signatures yessir (im fearful for how long these will take to implement)
    
    * Create recruiters.js
    
    with some of the signatures to be implemented later
    
    * Update mongoCollections.js
    
    added recruiters db into collections file
    
    * Update jobs.js
    
    wrote getalljobs, yet to test
    
    * Update jobs.js
    
    short note abt other functions we need to implement
    
    * Update jobs.js
    
    wrote remove
    
    * Update jobs.js
    
    edited the create function
    
    * Update jobs.js
    
    brain fart
    
    * Update applicants.js
    
    updated applicant create params
    
    * applicants create function progress graaa
    
    it aint much but its honest work
    
    * Create validation.js
    
    aughhhgh
    
    * Update applicants.js
    
    * jobs data functions update
    
    still yet to implement some things, and still yet to test all this mess
    
    * jobs db strategizing
    
    heheheh
    
    * jobs db strategizing
    
    heheheh
    
    * Update applicants.js
    
    planning to rewrite these functions using the validation helper functions
    
    * moved validation into data dir
    
    * Update applicants.js
    
    added favoriteJob signature
    
    * Update applicants.js
    
    changed the location of validation file, so i changed the import line
    
    * Update jobs.js
    
    changed the location of validation helper functions file, so i changed the import line
    
    * Update applicants.js
    
    wrote create function, input validation incomplete
    
    * changed some input validation aspects
    
    * Update validation.js
    
    * Update applicants.js
    
    update to create function, input validation not complete
    
    * progress progress
    
    little by little
    
    * Update applicants.js
    
    had a variable name wrong my b
    
    * Update applicants.js
    
    * var name error my b
    
    * Update applicants.js
    
    wrote remove function
    
    * Update jobs.js
    
    few input validation changes just want to push before i forget bc im thinking rn
    
    * Update recruiters.js
    
    * Update recruiters.js
    
    create function prep
    
    * Update recruiters.js
    
    fields etc
    
    * Update recruiters.js
    
    to match harshil's version from the main branch
    
    * Update validation.js
    
    added email validation
    
    * Update validation.js
    
    added email validation
    
    * Update recruiters.js
    
    i dont use a "helper" file, my validation helper functions are in the "validation" file
    
    * Update recruiters.js
    
    cleanin up
    
    * Update jobs.js
    
    workin on getJobsApplied
    
    * Update jobs.js
    
    cleanin up, gameplan comments etc
    
    * Update applicants.js
    
    rewrote applicant create function
    
    * Update applicants.js
    
    * Update jobs.js
    
    * file import update
    
    ---------
    
    Co-authored-by: HarshilCodes <38358248+HarshilCodes@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,54 +1,5 @@
 import {dbConnection, closeConnection} from './config/mongoConnection.js';
 import jobData from './data/jobs.js';
-import express from 'express';
-const app = express();
-import configRoutes from './routes/index.js';
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const rewriteUnsupportedBrowserMethods = (req, res, next) => {
-   if(req.body && req.body._method) {
-        req.method = req.body._method;
-        delete req.body._method;
-    }
-    next();
-};
-
-app.use(rewriteUnsupportedBrowserMethods);
-import session from 'express-session';
-import exphbs from 'express-handlebars';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const staticDir = express.static(__dirname + '/public');
-
-const handlebarsInstance = exphbs.create({
-    defaultLayout: 'main',
-    // Specify helpers which are only registered on this instance.
-    helpers: {
-      asJSON: (obj, spacing) => {
-        if (typeof spacing === 'number')
-          return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
-  
-        return new Handlebars.SafeString(JSON.stringify(obj));
-      }
-    }
-  });
-
-app.use('/public', staticDir);
-app.engine('handlebars', handlebarsInstance.engine);
-app.set('view engine', 'handlebars');
-app.use(session({
-    name: 'AuthCookie',
-    secret: 'some secret string!',
-    resave: false,
-    saveUninitialized: false
-  }));
-
-
 
 //lets drop the database each time this is run
 const db = await dbConnection();
@@ -58,52 +9,11 @@ await db.dropDatabase();
 let job1 = undefined;
 
 //next to test the create function
-// try {
-//     job1 = await jobData.create('software dev', 'Nokia', 'https://www.nokia.com', ['computer science', 'cyber security']);
-// } catch(e) {
-//     console.error('Could not add job1:',e);
-// }
-
-// middleware added to all routes, at this point the routes aren't all coded in yet so not sure what to name all these redirects to yet lol
-app.get('/', async (req, res, next) => {
-    if(req.session.user){
-        return res.redirect("/homepage");
-    }else{
-        return res.redirect("/login");
-    }
-    next();
-  });
-
-app.get('/login', async (req, res, next) => {
-    if(req.session.user){
-        return res.redirect("/homepage");
-    }
-    next();
-}); 
-
-app.get('/signup', async (req, res, next) => {
-    if(req.session.user){
-        return res.redirect("/homepage");
-    }
-    next();
-});
-
-app.get('/logout', async (req, res, next) => {
-    if(req.method === "GET"){
-        if(req.session.user){
-            
-        }else{
-            req.method = 'GET';
-            return res.redirect("/login");
-        }
-    }
-    next();
-
-});
-
-
-
-
+try {
+    job1 = await jobData.create('software dev', 'Nokia', 'https://www.nokia.com', ['computer science', 'cyber security']);
+} catch(e) {
+    console.error('Could not add job1:',e);
+}
 
 //finally to close up shop,, get back to the terminal after doing what u need to do in the db
 await closeConnection();
---
+++ data/validation.js
@@ -0,0 +1,48 @@
+import {ObjectId} from 'mongodb';
+
+//shouldve thought to make a helper functions file earlier huh
+const exportedMethods = {
+  checkId(id) {
+    if (!id) throw 'Error: You must provide an id to search for';
+    if (typeof id !== 'string') throw 'Error: id must be a string';
+    id = id.trim();
+    if (id.length === 0)
+      throw 'Error: id cannot be an empty string or just spaces';
+    if (!ObjectId.isValid(id)) throw 'Error: invalid object ID';
+    return id;
+  },
+
+  checkString(strVal, varName) {
+    if (!strVal) throw `Error: You must supply a ${varName}!`;
+    if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
+    strVal = strVal.trim();
+    if (strVal.length === 0)
+      throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+    if (!isNaN(strVal))
+      throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+    return strVal;
+  },
+
+  checkStringArray(arr, varName) {
+    //We will allow an empty array for this,
+    //if it's not empty, we will make sure all tags are strings
+    if (!arr || !Array.isArray(arr))
+      throw `You must provide an array of ${varName}`;
+    for (let i in arr) {
+      if (typeof arr[i] !== 'string' || arr[i].trim().length === 0) {
+        throw `One or more elements in ${varName} array is not a string or is an empty string`;
+      }
+      arr[i] = arr[i].trim();
+    }
+    return arr;
+  },
+
+  checkEmail(email) {
+    if (!email) throw "Email Address must be supplied";
+    let nEmailAddress = email.toLowerCase();
+    if (!(/\S+@\S+\.\S+/.test(nEmailAddress))) throw "Invalid email address.";
+    return nEmailAddress;
+  }
+};
+
+export default exportedMethods;
